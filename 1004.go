afbd99619a616702ef627962160f0ec0f1da3e7a
Alvaro Rosa Teixeira <alvaro.rosa1985@gmail.com>
2022-02-14 22:14:38 -0300

    Create 1004.go

---
+++ 1004.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt")
+
+func main() {
+	var a int
+	var b int
+	var PROD int
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	PROD = a * b
+	fmt.Printf("PROD = %d\n", PROD)
+}
