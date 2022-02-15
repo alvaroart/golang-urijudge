b2e6e2a96321486a70a08da3046a12441d4150f4
Alvaro Rosa Teixeira <alvaro.rosa1985@gmail.com>
2022-02-14 22:16:00 -0300

    Create 1003.go

---
+++ 1003.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt")
+
+func main() {
+	var a int
+	var b int
+	var SOMA int
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	SOMA = a + b
+	fmt.Printf("SOMA = %d\n", SOMA)
+}
